6d4d27f32bbf02264015cf4c8ec071ca1885f737
Megraj Chauhan <megrajchauhan123@gmail.com>
2024-08-28 09:19:58 +0545

    The dilemma of path length

--- includes/neuron.hpp
+++ includes/neuron.hpp
@@ -11,8 +11,10 @@ namespace neuron
    class Neuron;
 
    typedef void (*activation_func_t)(Neuron *);
+   typedef bool (*connect_func_t)(Neuron *);
+   typedef bool (*destroy_func_t)(Neuron *);
 
-   class Path
+   struct Path
    {
       Neuron *sender;
       Neuron *receiver;
@@ -27,14 +29,28 @@ namespace neuron
       double _dest_potential;
       double _dest_threshold;
       activation_func_t _activation_func;
-      std::vector<Neuron *> *_call_list;
+      std::unordered_map<size_t, bool> *_call_list;
       bool _io_neuron;
       std::vector<double> _inp_signals;
+      double _res_signal; // the result of the activation function
+      size_t ind = 0;
 
    public:
       Neuron() = default;
 
-      void init(double cp, double ct, double dp, double dt, std::vector<Neuron*> cl, activation_func_t f, bool is_io);
+      void init(size_t i, double cp, double ct, double dp, double dt, std::unordered_map<size_t, bool> *cl, activation_func_t f, bool is_io);
+
+      void activate();
+
+      void broadcast();
+
+      void accept_val(double _sig);
+
+      size_t get_ind();
+
+      void connect(Neuron *receiver, double _th);
+
+      void destroy_conn(size_t i);
    };
 };
 
--- src/neuron.cpp
+++ src/neuron.cpp
@@ -1,1 +1,56 @@
 #include "neuron.hpp"
+
+void neuron::Neuron::init(size_t i, double cp, double ct, double dp, double dt, std::unordered_map<size_t, bool> *cl, activation_func_t f, bool is_io)
+{
+    _conn_threshold = ct;
+    _conn_potential = cp;
+    _dest_potential = dp;
+    _dest_threshold = dt;
+    _activation_func = f;
+    _call_list = cl;
+    _io_neuron = is_io;
+    ind = i;
+}
+
+void neuron::Neuron::activate()
+{
+    _activation_func(this); // this should handle all of it
+}
+
+void neuron::Neuron::broadcast()
+{
+    // Go through each path and broadcast the resulting signal
+    // But only to those neurons who cross the threshold signal
+    for (auto _p : connections)
+    {
+        if (_p._send_threshold <= _res_signal)
+        {
+            _p.receiver->accept_val(_res_signal);
+            (*_call_list)[_p.receiver->get_ind()] = true;
+        }
+    }
+}
+
+void neuron::Neuron::accept_val(double _sig)
+{
+    _inp_signals.push_back(_sig);
+}
+
+size_t neuron::Neuron::get_ind()
+{
+    return ind;
+}
+
+void neuron::Neuron::connect(Neuron *receiver, double _th)
+{
+    Path p;
+    p.receiver = receiver;
+    p.sender = this;
+    p._send_threshold = _th;
+    connections.push_back(p);
+}
+
+void neuron::Neuron::destroy_conn(size_t i)
+{
+    connections.erase(connections.begin() + i);
+}
